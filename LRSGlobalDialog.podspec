fc63a7e03ec644a584ce7f5ae843cfd2ba1ec7de
junchen <junchen.liu@jiamiantech.com>
2024-04-16 11:15:28 +0800

    feat: API修改

--- LRSGlobalDialog.podspec
+++ LRSGlobalDialog.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'LRSGlobalDialog'
-  s.version          = '0.1.1'
+  s.version          = '0.1.2'
   s.summary          = 'A short description of LRSGlobalDialog.'
 
 # This description is used to generate tags and improve search results.
--- LRSGlobalDialog/Classes/LRSGlobalDialog.swift
+++ LRSGlobalDialog/Classes/LRSGlobalDialog.swift
@@ -8,17 +8,30 @@
 import UIKit
 import ReactiveObjC
 
-@objc public class LRSGlobalDialog: NSObject {
+@objc
+public class LRSGlobalDialog: NSObject {
 	@objc public static let `default` = LRSGlobalDialog()
 	private var presentedAlert: Set = Set<Int>()
-	@objc public func showAlertView(title: String?, code: Int, message: String?, confirmButtonTitle: String? = "知道了") {
+	private typealias AlertView = UIAlertView
+	public typealias AlertClickedCallBack = (Int) -> Void
+
+	@objc public func showAlertView(title: String? = "", code: Int, message: String? = "", confirmButtonTitle: String? = "知道了", cancelButtonTitle: String? = "取消", onButtonClicked: AlertClickedCallBack? = { _ in }) {
 		guard presentedAlert.contains(code) == false else {
 			return
 		}
-		let alert = UIAlertView(title: title, message: message, delegate: nil, cancelButtonTitle: confirmButtonTitle)
-		alert.show()
-		alert.rac_buttonClickedSignal().subscribeNext { (index) in
+		var alertView: AlertView!
+		if let confirmButtonTitle = confirmButtonTitle {
+			alertView = AlertView(title: title ?? "", message: message ?? "", delegate: nil, cancelButtonTitle: cancelButtonTitle, otherButtonTitles: confirmButtonTitle)
+		} else {
+			alertView = AlertView(title: title, message: message, delegate: nil, cancelButtonTitle: cancelButtonTitle)
+		}
+		alertView.show()
+		alertView.rac_buttonClickedSignal().subscribeNext { (index) in
 			self.presentedAlert.remove(code)
+			guard let callBack = onButtonClicked, let index = index as? Int else {
+				return
+			}
+			callBack(index)
 		}
 		self.presentedAlert.insert(code)
 	}
@@ -26,4 +39,11 @@ import ReactiveObjC
 	@objc public static func showAlertView(title: String?, code: Int, message: String?, confirmButtonTitle: String? = "知道了") {
 		LRSGlobalDialog.default.showAlertView(title: title, code: code, message: message, confirmButtonTitle: confirmButtonTitle)
 	}
+
+	@objc public static func debugDialog(title: String?, code: Int, message: String?, confirmButtonTitle: String? = "知道了") {
+		#if targetEnvironment(simulator) || DEBUG
+		LRSGlobalDialog.default.showAlertView(title: title, code: code, message: message, confirmButtonTitle: confirmButtonTitle)
+		#endif
+	}
+
 }
